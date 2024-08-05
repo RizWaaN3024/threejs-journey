a330a858721aeb4e23574437fd62311a648dbaf2
rizwaan-ansari <rizwaan@oromedialab.com>
2024-08-06 01:41:20 +0530

    Learnt object Transformations

---
+++ transformation-objects/src/script.js
@@ -0,0 +1,63 @@
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+// mesh.position.x = 2;
+// mesh.position.y = 1;
+// mesh.position.z = -1;
+mesh.position.set(2, 1, -1)
+mesh.position.normalize();
+scene.add(mesh)
+
+// Axes Helper
+
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
+// scale
+// mesh.scale.x = 2;
+// mesh.scale.y = 1;
+// mesh.scale.z = 2;
+mesh.scale.set(2, 1, 2);
+
+// rotation
+
+mesh.rotation.y = Math.PI * 0.5;
+mesh.rotation.x = Math.PI * 0.5;
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+camera.position.x = 1;
+camera.position.y = 1;
+camera.lookAt(mesh.position)
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
