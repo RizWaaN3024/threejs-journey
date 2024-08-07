0e2ce1f2a7f4ee0070b34f5c1a0aeb9f1fc5ee16
RizWaaN3024 <rizwaanans786@gmail.com>
2024-08-08 00:40:41 +0530

    Cameras

--- animations/src/script.js
+++ animations/src/script.js
@@ -7,7 +7,7 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
+const geometry = new THREE.BoxGeometry(1, 1, 1, 5, 5, 5)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
@@ -19,8 +19,12 @@ const sizes = {
 }
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+// const camera = new THREE.PerspectiveCamera(verticalFieldOfView, aspectRation, nearValue, farValue);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.01, 100)
+camera.position.x = 2;
+camera.position.y = 2;
+camera.position.z = 2;
+camera.lookAt(mesh.position)
 scene.add(camera)
 
 // Renderer
@@ -37,7 +41,7 @@ const tick = () => {
 
     // mesh.position.x += 0.01;
     mesh.rotation.y += 0.01;
-    mesh.rotation.x += 0.01;
+    // mesh.rotation.x += 0.01;
     renderer.render(scene, camera)
     window.requestAnimationFrame(tick)
 }
