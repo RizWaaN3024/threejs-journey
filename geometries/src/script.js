7b439e72fa1cc7dcddf17c1df0f09e150e5622ed
RizWaaN3024 <rizwaanans786@gmail.com>
2024-08-12 02:27:28 +0530

    Test

--- geometries/src/script.js
+++ geometries/src/script.js
@@ -1,9 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-/**
- * Base
- */
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
