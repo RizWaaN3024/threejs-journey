e1b428b167f82f470e1ae443168ada759dc72eee
rizwaan-ansari <rizwaan@oromedialab.com>
2024-08-12 01:36:43 +0530

    Learnt to handle pixel ratio

--- animations/src/script.js
+++ animations/src/script.js
@@ -31,7 +31,7 @@ window.addEventListener('resize', () => {
 
     // Update Renderer
     renderer.setSize(sizes.width, sizes.height)
-    
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 
