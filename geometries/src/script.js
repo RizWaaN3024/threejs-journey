0371ad4a634d59ee4a82cf563547effe3c089af2
RizWaaN3024 <rizwaanans786@gmail.com>
2024-08-14 01:19:46 +0530

    Test

--- geometries/src/script.js
+++ geometries/src/script.js
@@ -55,10 +55,8 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
     // Update controls
     controls.update()
-
     // Render
     renderer.render(scene, camera)
 
