a1a96e77003d189b6bb6410ce8adad17f7ebb33f
RizWaaN3024 <rizwaanans786@gmail.com>
2024-08-15 02:28:00 +0530

    Created custom geometry

--- geometries/src/script.js
+++ geometries/src/script.js
@@ -12,16 +12,25 @@ const scene = new THREE.Scene()
 // const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
 
 
-const positionsArray = new Float32Array([
-    0, 0, 0,
-    0, 1, 0,
-    1, 0, 0
-])
+// const positionsArray = new Float32Array([
+//     0, 0, 0,
+//     0, 1, 0,
+//     1, 0, 0
+// ])
 
-const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+// const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
 const geometry = new THREE.BufferGeometry();
-geometry.setAttribute('position', positionsAttribute)
+// geometry.setAttribute('position', positionsAttribute)
+
+const count = 50;
+const positionsArray = new Float32Array(count * 3 * 3)
+
+for(let i = 0; i < count * 3 * 3; i++) {
+    positionsArray[i] = Math.random() - 0.5
+}
     
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute('position', positionsAttribute)
 
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
