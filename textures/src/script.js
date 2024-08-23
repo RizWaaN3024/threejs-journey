d70b3a34377c6d2fd8f025c68dcc8e210b46aeaa
RizWaaN3024 <rizwaanans786@gmail.com>
2024-08-24 01:06:51 +0530

    Added Texture

--- textures/src/script.js
+++ textures/src/script.js
@@ -1,6 +1,18 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
+
+// Textures
+
+const image = new Image()
+const texture = new THREE.Texture(image)
+texture.colorSpace = THREE.SRGBColorSpace
+
+image.onload = () => {
+    texture.needsUpdate = true;
+}
+
+image.src = '/textures/door/color.jpg'
 /**
  * Base
  */
@@ -14,7 +26,7 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ map: texture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
