e0d98349eb10a49909a143b558a2e35d4f3e256d
RizWaaN3024 <rizwaanans786@gmail.com>
2024-08-28 01:33:03 +0530

    Added different textures

--- materials/src/script.js
+++ materials/src/script.js
@@ -14,7 +14,18 @@ const scene = new THREE.Scene()
 
 const textureLoader = new THREE.TextureLoader();
 
-const doorColorTexture = TextureLoader.load('./textures/door/color.jpg')
+const doorColorTexture = textureLoader.load('./textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('./textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('./textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('./textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('./textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('./textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('./textures/door/roughness.jpg')
+const matcapTexture = textureLoader.load('./textures/matcaps/1.png')
+const gradientTexture = textureLoader.load('./textures/gradients/3.jpg')
+
+doorColorTexture.colorSpace = THREE.SRGBColorSpace
+matcapTexture.colorSpace = THREE.SRGBColorSpace
 
 // objects
 
